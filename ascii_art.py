2905a3ec92524e1e89e3f0d22b460107cc306136
Friedemann Muecket <f.muecket@gmail.com>
2025-08-15 18:08:25 +0200

    modified ascii_art stages; added input validation; added clear display, added replay option

--- ascii_art.py
+++ ascii_art.py
@@ -10,20 +10,34 @@ STAGES = [
      """,
      # Stage 1: Bottom part starts melting
      """
+     
       ___  
      /___\\ 
      (o o) 
-     ( : ) 
+     ( : )_
      """,
      # Stage 2: Only the head remains
      """
+     
+     
       ___  
      /___\\ 
-     (o o) 
+     (o o) __
      """,
      # Stage 3: Snowman completely melted
      """
+     
+     
+     
       ___  
-     /___\\ 
+     /___\\ ___
+     """,
+     # Stage 4: Snowman completely melted
+     """
+     
+     
+     
+      
+     ___________
      """
  ]
--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,4 @@
-import random
+import random, os, platform, sys
 from ascii_art import STAGES
 from secret_words import WORDS
 
@@ -8,42 +8,92 @@ def get_random_word():
 
 
 def display_game_state(mistakes, secret_word, guessed_letters):
+    """
+    Takes in mistakes, secret word and a list of guessed letters
+    Shows stage of Ascii art snowman
+    compare guessed letters with secret word
+    display correct letters
+    """
     # Display the snowman stage
     print(STAGES[mistakes])
     display_word = ""
-    letter_count = 0
-
-    # Display letters
+    # check if letter in secret word and display letter
     for letter in secret_word:
         if letter in guessed_letters:
             display_word += letter + " "
-            letter_count += 1
         else:
             display_word += "_ "
             mistakes += 1
-
     print(f"Word: {display_word}\n")
+
+
+def validate_input():
+    """prompts user to enter a guess. checks if input is just one letter and returns user input"""
+    while True:
+        user_input = input("Guess a letter: ").lower()
+        if len(user_input) == 1 and user_input.isalpha():
+            print("You guessed:", user_input)
+            return user_input
+        else:
+            print("Invalid input! Please enter a single letter.")
+
+
+def clear_console():
+    """clears console (only works in the (real) terminal)"""
+    # Check system and clear the Screen
+    if platform.system == "Windows":
+        os.system("cls")
+    else:
+        os.environ.setdefault("TERM", "xterm")
+        os.system("clear")
+
+
+
+def check_final_conditions(guessed_letters, secret_word, mistakes):
+    """checks whether the guessed correct letters correspond to the number of letters in the secret word."""
+    # check whether the player won or lost
+    letter_count = 0
+    for letter in secret_word:
+        if letter in guessed_letters:
+            letter_count += 1
     if letter_count == len(secret_word):
         print("You won!")
-        exit()
+    elif mistakes >= 4:
+        print("Game over!")
+    else:
+        return None
 
+    # Ask user to replay
+    while True:
+        user_choice = input("Want to play again (y/n): ")
+        if user_choice.lower() == "y":
+            play_game()
+        elif user_choice.lower() == "n":
+            exit()
+        else:
+            print("Invalid input. Please enter y or n")
 
 
 def play_game():
+    """executes the game logic"""
     mistakes = 0
     guessed_letters = []
     secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print(f"secret word: {secret_word}") # for testing
-
 
     while True:
+        # clear display
+        clear_console()
+        print("Welcome to Snowman Meltdown!")
+        #print(f"secret word: {secret_word}")  # for testing
+
         # Display game state
         display_game_state(mistakes, secret_word, guessed_letters)
 
-        # For now, simply prompt the user once:
-        guess = input("Guess a letter: ").lower()
-        print("You guessed:", guess)
+        # check if game is over
+        check_final_conditions(guessed_letters, secret_word, mistakes)
+
+        # get user input and validate input
+        guess = validate_input()
 
         # add guess to list of guessed letters
         guessed_letters.append(guess)
@@ -52,7 +102,3 @@ def play_game():
         if guess not in secret_word:
             mistakes += 1
 
-        # check if game is over
-        if mistakes >= 3:
-            print("Game over!")
-            exit()
